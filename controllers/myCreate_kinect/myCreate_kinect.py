a68554ff2422ce40fb7df50212f02bd00953385c
JordenHuang <jorden90573@gmail.com>
2025-08-01 14:39:11 +0800

    Add Inertial unit

--- controllers/myCreate_kinect/myCreate_kinect.py
+++ controllers/myCreate_kinect/myCreate_kinect.py
@@ -1,8 +1,10 @@
 from controller import Robot
 from controller import Keyboard
-import cv2
-import numpy as np
-import os
+from controller import Gyro
+# import cv2
+# import numpy as np
+# import os
+import math
 
 
 # Create the robot instance
@@ -32,8 +34,41 @@ camera_range = myCreate.getDevice("kinect range")
 camera.enable(TIMESTEP)
 camera_range.enable(TIMESTEP)
 
+inertial_unit = myCreate.getDevice("inertial unit")
+inertial_unit.enable(TIMESTEP)
+
 camera_width = camera.getWidth()
 camera_height = camera.getHeight()
+
+def getIMUDegrees(): #convert radians to degrees and make range 180
+    return inertial_unit.getRollPitchYaw()[2] * 180/math.pi
+
+def getDirectionFacing():
+    degrees = (getIMUDegrees() + 360) % 360
+    if(degrees < 22.5 or degrees > 337.5):
+        return 'N'
+    if(degrees < 337.5 and degrees > 292.5):
+        return 'NE'
+    if(degrees < 292.5 and degrees > 247.5):
+        return 'E'
+    if(degrees < 247.5 and degrees > 202.5):
+        return 'SE'
+    if(degrees < 202.5 and degrees > 157.5):
+        return 'S'
+    if(degrees < 157.5 and degrees > 112.5):
+        return 'SW'
+    if(degrees < 112.5 and degrees > 67.5):
+        return 'W'
+    if(degrees < 67.5 and degrees > 22.5):
+        return 'NW'
+    return '?'
+
+def IMUPrint(): # print robot orientation in degrees
+    print(' ')
+    print('[IMU: '+ str(round(getIMUDegrees(), 1)) + '° ' + getDirectionFacing() + ']')
+    # dist = getDistanceSensors()
+    # print('[left, front, right] | ', end='')
+    # print(dist)
 
 # Main loop
 while myCreate.step(TIMESTEP) != -1:
@@ -55,3 +90,5 @@ while myCreate.step(TIMESTEP) != -1:
     else:
         left_motor.setVelocity(0.0)
         right_motor.setVelocity(0.0)
+
+    IMUPrint()
